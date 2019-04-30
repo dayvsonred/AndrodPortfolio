1dbadaf851d6b1f3b237cadbf2b5bc1c59970a0e
Dayvson <dayvson.red@gmail.com>
2019-04-30 00:14:12 -0300

    conect ok

--- app/src/main/java/com/example/dayvsonandrodportfolio/MainActivity.java
+++ app/src/main/java/com/example/dayvsonandrodportfolio/MainActivity.java
@@ -1,18 +1,28 @@
 package com.example.dayvsonandrodportfolio;
 
 import android.content.Intent;
+import android.database.SQLException;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.support.constraint.ConstraintLayout;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.Button;
 
+import com.example.dayvsonandrodportfolio.database.DadosOpenHelper;
+
 public class MainActivity extends AppCompatActivity {
 
 
     private Button ButtonOpenCalculadora;
+    private SQLiteDatabase conexao;
+    private DadosOpenHelper dadosOpenHelper;
+
+    private ConstraintLayout layoutContatinMain;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,7 +42,29 @@ public class MainActivity extends AppCompatActivity {
 
 
         ButtonOpenCalculadora = findViewById(R.id.buttonOpenCalculardora);
+        layoutContatinMain = (ConstraintLayout) findViewById(R.id.main_content_actv);
 
+        CreateConexao();
+
+    }
+
+    private void CreateConexao(){
+
+        try {
+            dadosOpenHelper = new DadosOpenHelper(this);
+            conexao = dadosOpenHelper.getWritableDatabase();
+            Snackbar.make(layoutContatinMain, "Conexão criada com sucesso",Snackbar.LENGTH_SHORT)
+                    .setAction("ok", null).show();
+
+        }catch (SQLException ex){
+            AlertDialog.Builder dlg = new AlertDialog.Builder(this);
+            dlg.setTitle("Erro");
+            dlg.setMessage(ex.getMessage());
+            dlg.setNeutralButton("ok", null);
+            dlg.show();
+
+
+        }
 
     }
 
